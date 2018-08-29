9ff1bede318551743c1ec6284f5cb670d46ac6c6
机智的胖纸 <sijie@jizhidepangzhideMac-mini.local>
2018-08-29 19:41:42 +0800

    基础模块

---
+++ base/base.py
@@ -0,0 +1,43 @@
+import influxdb
+import time,os
+import configparser
+
+
+class Base(object):
+    def __init__(self,configfile="../config/conf.ini"):
+        self.configfile = configfile
+        self.parser = self.configparser()
+        self.client = influxdb.InfluxDBClient(self.parser.get("server","host"),self.parser.getint("server","port"),self.parser.get("server","username"),self.parser.get("server","password"),self.parser.get("server","dbname"))
+
+    def configparser(self):
+        if os.path.exists(self.configfile) is True:
+            self.configfile = "../config/conf.ini"
+        cf = configparser.ConfigParser()
+        cf.read(self.configfile)
+        return cf
+
+    def average(self,metricname="cpu",group=None,host=None):
+        if group is None and host is None:
+            sql = "select mean(*) from "+metricname
+        if group is None and host is not None:
+            sql = "select mean(*) from "+metricname+" where \"hostname\" = '%s'" %(host)
+        if group is not None and host is None:
+            sql = "select mean(*) from "+metricname+" where \"groupname\" = '%s'" %(group)
+        if group is not None and host is not None:
+            sql = "select mean(*) from " + metricname + " where \"groupname\" = '%s' and \"hostname\" = '%s'" % (group,host)
+
+    def write(self):
+        pass
+
+    def execute(self,sql):
+        try:
+            data = self.client.query(sql).raw
+        except Exception as e:
+            print(str(e))
+            data = None
+        return data
+
+
+if __name__ == "__main__":
+    b = Base()
+    print(b.parser.get("cpu","threshold"))
---
+++ base/db.py
@@ -0,0 +1,23 @@
+import pymysql
+
+class Mysql(object):
+    def __init__(self):
+        self.db = self._connect()
+
+    def _connect(self):
+        db = pymysql.connect(host="localhost", user="root", password="", db="sijie", port=3306)
+        return db
+
+    def _close(self):
+        self.db.close()
+
+    def write(self,sql):
+        cur = self.db.cursor()
+        try:
+            cur.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+
+    def read(self,sql):
+        pass
